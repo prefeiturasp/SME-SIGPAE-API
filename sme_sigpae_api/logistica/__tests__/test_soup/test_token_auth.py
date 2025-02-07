f9b005d6237469a4d85b3ddad0a0c5598a8c83df
Priscyla C. dos Santos <santospriscyla@gmail.com>
2025-02-07 18:15:31 -0300

    test(125658): Inclui test do arquivo token.auth do app logistica

---
+++ sme_sigpae_api/logistica/__tests__/test_soup/test_token_auth.py
@@ -0,0 +1,76 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+from rest_framework import exceptions
+from rest_framework.authtoken.models import Token
+
+from sme_sigpae_api.logistica.api.soup.token_auth import TokenAuthentication
+
+pytestmark = pytest.mark.django_db
+
+
+def test_get_model(token_valido):
+    usuario, token, model = token_valido
+    token_auth = TokenAuthentication()
+    token_auth.model = model
+    assert token_auth.get_model() == model
+
+
+def test_get_model_default():
+    token_auth = TokenAuthentication()
+    assert token_auth.get_model() == Token
+
+
+def test_authenticate_token_valido(token_valido):
+    usuario, token, model = token_valido
+    token_auth = TokenAuthentication()
+    user, token_ok = token_auth.authenticate_credentials(model.StrToken)
+    assert user == usuario
+    assert token_ok == token
+
+
+def test_authenticate_falha_sem_modelo():
+    with pytest.raises(
+        exceptions.AuthenticationFailed,
+        match="Token inválido. Não foi enviado informações de credenciais.",
+    ):
+        TokenAuthentication().authenticate(None)
+
+
+def test_authenticate_falha_no_strtoken(token_valido):
+    usuario, token, model = token_valido
+    with pytest.raises(
+        exceptions.AuthenticationFailed,
+        match="Falha na autenticação. StrToken não foi informado.",
+    ):
+        TokenAuthentication().authenticate(usuario)
+
+
+def test_authenticate_credentials(token_valido):
+    usuario, token, model = token_valido
+    token_auth = TokenAuthentication()
+    with patch.object(
+        token_auth, "authenticate_credentials", return_value=(usuario, token)
+    ) as mock_auth_cred:
+        user, token_ok = token_auth.authenticate(model)
+
+    mock_auth_cred.assert_called_once_with(model.StrToken)
+    assert user == usuario
+    assert token_ok == token
+
+
+def test_authenticate_credentials_invalida():
+    with pytest.raises(exceptions.AuthenticationFailed, match="Token inválido."):
+        TokenAuthentication().authenticate_credentials(122)
+
+
+def test_authenticate_credentials_usuario_inativo(token_valido):
+    usuario, token, model = token_valido
+    token.model = MagicMock()
+    usuario.is_active = False
+    usuario.save()
+
+    with pytest.raises(
+        exceptions.AuthenticationFailed, match="Usuário inativo ou deletado."
+    ):
+        TokenAuthentication().authenticate_credentials(model.StrToken)
