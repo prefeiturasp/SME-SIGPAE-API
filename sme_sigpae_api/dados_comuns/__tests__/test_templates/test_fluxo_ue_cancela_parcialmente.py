9c41e2c5f14f5979677ff4e97f20f1d4279c9a52
Priscyla C. dos Santos <santospriscyla@gmail.com>
2025-02-17 16:58:05 -0300

    test(125318): testa os e-mails reponsaveis pelos fluxos da parte de alimentacao

---
+++ sme_sigpae_api/dados_comuns/__tests__/test_templates/test_fluxo_ue_cancela_parcialmente.py
@@ -0,0 +1,25 @@
+from django.template.loader import render_to_string
+
+
+def test_template_email_base():
+    titulo = "Teste template"
+    template = "fluxo_ue_cancela_parcialmente.html"
+    hidden_email = None
+    dados_template = {
+        "titulo": titulo,
+        "hidden_email": hidden_email,
+    }
+
+    html = render_to_string(template, context=dados_template)
+    assert isinstance(html, str)
+
+    assert "<title>SIGPAE - Notificação</title>" in html
+    assert "<p>smecodaesigpae@sme.prefeitura.sp.gov.br</p>" in html
+    assert f"<b>{titulo}</b>" in html
+    assert (
+        'src="https://hom-sigpae.sme.prefeitura.sp.gov.br/django_static/images/email/email-sigpae.png'
+        in html
+    )
+    assert (
+        "<strong>foi parcialmente cancelada</strong> pela Unidade Educacional:" in html
+    )
