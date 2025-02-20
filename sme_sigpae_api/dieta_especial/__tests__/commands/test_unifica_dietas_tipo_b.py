d5deb6e751c7050b4beb567aa63f5a46fcf1620c
Calvin Rossinhole <calvin.masters@gmail.com>
2025-02-20 15:33:05 -0300

    test(125683): :white_check_mark: adiciona caso log dietas

--- sme_sigpae_api/dieta_especial/__tests__/commands/test_unifica_dietas_tipo_b.py
+++ sme_sigpae_api/dieta_especial/__tests__/commands/test_unifica_dietas_tipo_b.py
@@ -1,3 +1,4 @@
+import datetime
 from unittest import TestCase
 
 import pytest
@@ -5,9 +6,20 @@ from django.core.management import call_command
 
 from sme_sigpae_api.dieta_especial.fixtures.factories.dieta_especial_base_factory import (
     ClassificacaoDietaFactory,
+    LogQuantidadeDietasAutorizadasCEIFactory,
+    LogQuantidadeDietasAutorizadasFactory,
     SolicitacaoDietaEspecialFactory,
 )
-from sme_sigpae_api.dieta_especial.models import SolicitacaoDietaEspecial
+from sme_sigpae_api.dieta_especial.models import (
+    LogQuantidadeDietasAutorizadas,
+    LogQuantidadeDietasAutorizadasCEI,
+    SolicitacaoDietaEspecial,
+)
+from sme_sigpae_api.escola.fixtures.factories.escola_factory import (
+    EscolaFactory,
+    FaixaEtariaFactory,
+    PeriodoEscolarFactory,
+)
 
 
 class UnificaDietasTipoBCommandTest(TestCase):
@@ -46,11 +58,60 @@ class UnificaDietasTipoBCommandTest(TestCase):
             classificacao=self.classificacao_tipo_b_lanche_refeicao
         )
 
+    def setup_logs_dietas_autorizadas(self):
+        self.periodo_integral = PeriodoEscolarFactory.create(nome="INTEGRAL")
+        self.escola = EscolaFactory.create(
+            nome="EMEF JARBAS SANTOS", codigo_eol="123456"
+        )
+
+        for dia in [1, 2, 3, 4, 5]:
+            LogQuantidadeDietasAutorizadasFactory.create(
+                classificacao=self.classificacao_tipo_b_lanche,
+                periodo_escolar=self.periodo_integral,
+                quantidade=10,
+                escola=self.escola,
+                data=datetime.date(2025, 2, dia),
+            )
+
+        for dia in [1, 2, 3, 4, 5]:
+            LogQuantidadeDietasAutorizadasFactory.create(
+                classificacao=self.classificacao_tipo_b_lanche_refeicao,
+                periodo_escolar=self.periodo_integral,
+                quantidade=20,
+                escola=self.escola,
+                data=datetime.date(2025, 2, dia),
+            )
+
+    def setup_logs_dietas_autorizadas_cei(self):
+        faixa_etaria = FaixaEtariaFactory.create()
+
+        for dia in [1, 2, 3, 4, 5]:
+            LogQuantidadeDietasAutorizadasCEIFactory.create(
+                classificacao=self.classificacao_tipo_b_lanche,
+                periodo_escolar=self.periodo_integral,
+                quantidade=10,
+                escola=self.escola,
+                data=datetime.date(2025, 2, dia),
+                faixa_etaria=faixa_etaria,
+            )
+
+        for dia in [1, 2, 3, 4, 5]:
+            LogQuantidadeDietasAutorizadasCEIFactory.create(
+                classificacao=self.classificacao_tipo_b_lanche_refeicao,
+                periodo_escolar=self.periodo_integral,
+                quantidade=20,
+                escola=self.escola,
+                data=datetime.date(2025, 2, dia),
+                faixa_etaria=faixa_etaria,
+            )
+
     def setUp(self) -> None:
         self.setup_dietas()
 
     @pytest.mark.django_db(transaction=True)
     def test_unifica_dietas_tipo_b(self) -> None:
+        self.setup_logs_dietas_autorizadas()
+        self.setup_logs_dietas_autorizadas_cei()
         self.call_command()
 
         assert (
@@ -59,3 +120,27 @@ class UnificaDietasTipoBCommandTest(TestCase):
             ).count()
             == 6
         )
+
+        assert (
+            LogQuantidadeDietasAutorizadas.objects.filter(
+                classificacao__nome="Tipo B"
+            ).count()
+            == 5
+        )
+        assert list(
+            LogQuantidadeDietasAutorizadas.objects.filter(
+                classificacao__nome="Tipo B"
+            ).values_list("quantidade", flat=True)
+        ) == [30, 30, 30, 30, 30]
+
+        assert (
+            LogQuantidadeDietasAutorizadasCEI.objects.filter(
+                classificacao__nome="Tipo B"
+            ).count()
+            == 5
+        )
+        assert list(
+            LogQuantidadeDietasAutorizadasCEI.objects.filter(
+                classificacao__nome="Tipo B"
+            ).values_list("quantidade", flat=True)
+        ) == [30, 30, 30, 30, 30]
