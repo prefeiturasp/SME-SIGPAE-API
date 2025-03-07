94eb52ad738086ee0696af86ed4385a10a46cc65
Calvin Rossinhole <calvin.masters@gmail.com>
2025-03-07 10:23:17 -0300

    test(126038): :white_check_mark: implementa testes unitarios historico matricula

--- sme_sigpae_api/escola/__tests__/commands/test_atualiza_alunos_escolas.py
+++ sme_sigpae_api/escola/__tests__/commands/test_atualiza_alunos_escolas.py
@@ -10,7 +10,9 @@ from rest_framework import status
 
 from sme_sigpae_api.escola.__tests__.conftest import mocked_response
 from sme_sigpae_api.escola.fixtures.factories.escola_factory import (
+    AlunoFactory,
     EscolaFactory,
+    HistoricoMatriculaAlunoFactory,
     PeriodoEscolarFactory,
 )
 from sme_sigpae_api.escola.management.commands.atualiza_alunos_escolas import (
@@ -29,7 +31,9 @@ class AtualizaAlunosEscolasCommandTest(TestCase):
         )
 
     def set_up_periodos_escolares(self):
-        PeriodoEscolarFactory.create(nome="MANHA", tipo_turno=1)
+        self.periodo_escolar_manha = PeriodoEscolarFactory.create(
+            nome="MANHA", tipo_turno=1
+        )
         PeriodoEscolarFactory.create(nome="INTERMEDIARIO", tipo_turno=2)
         PeriodoEscolarFactory.create(nome="TARDE", tipo_turno=3)
         PeriodoEscolarFactory.create(nome="VESPERTINO", tipo_turno=4)
@@ -103,6 +107,20 @@ class AtualizaAlunosEscolasCommandTest(TestCase):
         mock_get_response_alunos_por_escola,
     ) -> None:
         self.setup_escola2()
+
+        aluno_7924897 = AlunoFactory.create(
+            codigo_eol="7924897",
+            periodo_escolar=self.periodo_escolar_manha,
+            escola=self.escola,
+        )
+        HistoricoMatriculaAlunoFactory.create(aluno=aluno_7924897, escola=self.escola)
+
+        AlunoFactory.create(
+            codigo_eol="8030441",
+            periodo_escolar=self.periodo_escolar_manha,
+            escola=self.escola,
+        )
+
         mock_get_response_alunos_por_escola.side_effect = [
             self.mocked_response_dados_alunos,
             mocked_response({}, 404),
@@ -110,7 +128,7 @@ class AtualizaAlunosEscolasCommandTest(TestCase):
             mocked_response({}, 404),
         ]
         self.call_command()
-        assert Aluno.objects.count() == 288
+        assert Aluno.objects.count() == 289
 
         aluno_davi = Aluno.objects.get(nome="DAVI LUCAS THOMAZ NASCIMENTO")
         assert aluno_davi.nao_matriculado is False
@@ -122,6 +140,45 @@ class AtualizaAlunosEscolasCommandTest(TestCase):
 
         historico_escola_000094 = aluno_davi.historico.get(escola=self.escola2)
         assert historico_escola_000094.data_fim is None
+
+        aluno_kimberlly = Aluno.objects.get(codigo_eol="8030441")
+        assert aluno_kimberlly.historico.exists() is True
+
+        aluno_theo = Aluno.objects.get(codigo_eol="7924897")
+        assert aluno_theo.historico.count() == 1
+        assert aluno_theo.historico.get().data_fim is not None
+
+        assert Aluno.objects.filter(nome="ZOE MOURA VIANA CARDOSO").exists() is False
+
+    @freeze_time("2025-01-01")
+    @patch(
+        "sme_sigpae_api.escola.management.commands.atualiza_alunos_escolas.Command.get_response_alunos_por_escola"
+    )
+    @pytest.mark.django_db(transaction=True)
+    def test_command_atualiza_alunos_escolas_d_menos_1_rematricula(
+        self,
+        mock_get_response_alunos_por_escola,
+    ) -> None:
+        self.setup_escola2_matricula_concluida()
+        mock_get_response_alunos_por_escola.side_effect = [
+            self.mocked_response_dados_alunos,
+            mocked_response({}, 404),
+            self.mocked_response_dados_alunos_2,
+            mocked_response({}, 404),
+        ]
+        self.call_command()
+        assert Aluno.objects.count() == 288
+
+        aluno_davi = Aluno.objects.get(nome="DAVI LUCAS THOMAZ NASCIMENTO")
+        assert aluno_davi.nao_matriculado is False
+        assert aluno_davi.escola == self.escola
+
+        assert aluno_davi.historico.count() == 2
+        historico_escola_000086 = aluno_davi.historico.get(escola=self.escola)
+        assert historico_escola_000086.data_fim is None
+
+        historico_escola_000094 = aluno_davi.historico.get(escola=self.escola2)
+        assert historico_escola_000094.data_fim is not None
 
         assert Aluno.objects.filter(nome="ZOE MOURA VIANA CARDOSO").exists() is False
 
