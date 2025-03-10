e77283d072197fda7cffb92962b2dfd2fcc3c70e
vferreira90 <vef_90kl@hotmail.com>
2025-03-10 14:27:42 -0300

    bugfix(126335): alteracao nomenclatura ADMINISTRADOR_CONTRATOS

--- sme_sigpae_api/perfil/management/commands/carga_perfis_vinculados.py
+++ sme_sigpae_api/perfil/management/commands/carga_perfis_vinculados.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 from sme_sigpae_api.perfil.models.perfil import Perfil, PerfisVinculados
 
 from ....dados_comuns.constants import (
-    ADMINISTRADOR_DICAE,
+    ADMINISTRADOR_CONTRATOS,
     ADMINISTRADOR_DIETA_ESPECIAL,
     ADMINISTRADOR_GESTAO_PRODUTO,
     ADMINISTRADOR_SUPERVISAO_NUTRICAO,
@@ -30,7 +30,7 @@ class Command(BaseCommand):
         )
         self.cria_perfil_vinculado(
             COORDENADOR_CODAE_DILOG_LOGISTICA,
-            [ADMINISTRADOR_DICAE, DILOG_ABASTECIMENTO],
+            [ADMINISTRADOR_CONTRATOS, DILOG_ABASTECIMENTO],
         )
 
     def cria_perfil_vinculado(self, master, subordinados):
