881326d0d2585c66217f57d666209d5662fa12f6
Priscyla C. dos Santos <santospriscyla@gmail.com>
2025-07-08 15:07:37 -0300

    test(129080): testa a transação ue_envia_sem_lancamentos

--- sme_sigpae_api/dados_comuns/__tests__/test_fluxo_status.py
+++ sme_sigpae_api/dados_comuns/__tests__/test_fluxo_status.py
@@ -1,7 +1,12 @@
 import pytest
 import xworkflows
+from rest_framework.exceptions import PermissionDenied, ValidationError
 
-from sme_sigpae_api.dados_comuns.fluxo_status import ReclamacaoProdutoWorkflow
+from sme_sigpae_api.dados_comuns.fluxo_status import (
+    ReclamacaoProdutoWorkflow,
+    SolicitacaoMedicaoInicialWorkflow,
+)
+from sme_sigpae_api.dados_comuns.models import LogSolicitacoesUsuario
 
 pytestmark = pytest.mark.django_db
 
@@ -30,3 +35,58 @@ def test_envia_email_recusa_reclamacao(dados_log_recusa):
     _, reclamacao_produto, log_recusa = dados_log_recusa
     reclamacao_produto._envia_email_recusa_reclamacao(log_recusa)
     assert reclamacao_produto.status == ReclamacaoProdutoWorkflow.CODAE_RECUSOU
+
+
+def test_ue_envia_sem_lancamentos(medicao_sem_lancamentos, user_diretor_escola):
+    usuario, _ = user_diretor_escola
+    justificativa = "Não houve aulas no período devido a reformas na escola."
+    kwargs = {
+        "user": usuario,
+        "justificativa_sem_lancamentos": justificativa,
+    }
+
+    medicao_sem_lancamentos.ue_envia_sem_lancamentos(**kwargs)
+    assert (
+        medicao_sem_lancamentos.status
+        == SolicitacaoMedicaoInicialWorkflow.MEDICAO_APROVADA_PELA_CODAE
+    )
+
+    medicao = medicao_sem_lancamentos.medicoes.first()
+    assert (
+        medicao.status
+        == SolicitacaoMedicaoInicialWorkflow.MEDICAO_EM_ABERTO_PARA_PREENCHIMENTO_UE
+    )
+
+    assert medicao_sem_lancamentos.logs.count() == 1
+    log = medicao_sem_lancamentos.logs.first()
+    assert log.status_evento == LogSolicitacoesUsuario.MEDICAO_APROVADA_PELA_CODAE
+    assert log.usuario == usuario
+    assert log.justificativa == justificativa
+
+
+def test_ue_envia_sem_lancamentos_usuario_sem_permissao(
+    medicao_sem_lancamentos, user_codae_produto
+):
+    kwargs = {
+        "user": user_codae_produto,
+        "justificativa_sem_lancamentos": "Não houve aulas no período devido a reformas na escola.",
+    }
+    with pytest.raises(
+        PermissionDenied, match="Você não tem permissão para executar essa ação."
+    ):
+        medicao_sem_lancamentos.ue_envia_sem_lancamentos(**kwargs)
+
+
+def test_ue_envia_sem_lancamentos_erro_medicao(
+    medicao_sem_lancamentos, user_diretor_escola
+):
+    usuario, _ = user_diretor_escola
+    kwargs = {
+        "user": usuario,
+        "justificativa_sem_lancamentos": "Não houve aulas no período devido a reformas na escola.",
+    }
+    medicao = medicao_sem_lancamentos.medicoes.first()
+    with pytest.raises(
+        ValidationError, match=r"`Medicao` não possui fluxo `ue_envia_sem_lancamentos`"
+    ):
+        medicao.ue_envia_sem_lancamentos(**kwargs)
