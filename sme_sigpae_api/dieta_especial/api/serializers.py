637ca8eaaee6f0be3d3d520431d9cff15db0a996
Marcus V. M. Martins <57776263+MarcusMartins38@users.noreply.github.com>
2025-07-30 11:48:40 -0400

    Revert "Hotfix/131214 informacoes divergentes consulta de dieta do aluno"

--- sme_sigpae_api/dieta_especial/api/serializers.py
+++ sme_sigpae_api/dieta_especial/api/serializers.py
@@ -394,7 +394,12 @@ class SolicitacoesAtivasInativasPorAlunoSerializer(serializers.Serializer):
 
     def get_ativas(self, obj):
         return obj.dietas_especiais.filter(
-            status__in=["CODAE_AUTORIZADO"],
+            status__in=[
+                "CODAE_AUTORIZADO",
+                "CODAE_AUTORIZOU_INATIVACAO",
+                "TERMINADA_AUTOMATICAMENTE_SISTEMA",
+                "CANCELADO_ALUNO_NAO_PERTENCE_REDE",
+            ],
             ativo=True,
         ).count()
 
