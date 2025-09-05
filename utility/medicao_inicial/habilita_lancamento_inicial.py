ecb0db08038d538ef2ff2f849c5c68630cbd96a5
Priscyla C. dos Santos <santospriscyla@gmail.com>
2025-09-05 12:55:39 -0300

    feat(132491): inclui log de alunos mariculados por período escolar

--- utility/medicao_inicial/habilita_lancamento_inicial.py
+++ utility/medicao_inicial/habilita_lancamento_inicial.py
@@ -13,11 +13,27 @@ DIAS_MES = 31
 QUANTIDADE_ALUNOS = 25
 
 
-
 def habilitar_dias_letivos():
     from sme_sigpae_api.escola.utils import calendario_sgp
     d = datetime.date(ANO, MES, 1)
     calendario_sgp(d)
+
+
+def incluir_log_alunos_matriculados(periodos, escola):
+    from sme_sigpae_api.escola.models import LogAlunosMatriculadosPeriodoEscola, PeriodoEscolar
+    for periodo in periodos:
+        pe = PeriodoEscolar.objects.get(nome=periodo)
+        for dia in range(1, DIAS_MES + 1):
+            log = LogAlunosMatriculadosPeriodoEscola(
+                escola=escola,
+                periodo_escolar=pe,
+                quantidade_alunos=QUANTIDADE_ALUNOS
+            )
+            log.save()
+            data = datetime.date(ANO, MES, dia)
+            LogAlunosMatriculadosPeriodoEscola.objects.filter(id=log.id).update(criado_em=data)
+        print(f"Logs do Período {periodo} cadastrados")
+
 
 if __name__ == "__main__":
     habilitar_dias_letivos()
