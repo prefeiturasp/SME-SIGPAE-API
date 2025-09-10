f3128611f8dbd1987ccc55134681c124d45570cf
Marcus Martins <marcusmartins38@gmail.com>
2025-09-10 15:58:39 -0400

    test(133723): validacao da criacao de ValorMedicao apenas com faixa_etaria

--- sme_sigpae_api/medicao_inicial/__tests__/conftest.py
+++ sme_sigpae_api/medicao_inicial/__tests__/conftest.py
@@ -619,6 +619,24 @@ def make_solicitacao_medicao_inicial(escola):
 
     return handle
 
+@pytest.fixture
+def solicitacao_medicao_inicial_sem_valores(escola):
+    periodo_manha = baker.make("PeriodoEscolar", nome="MANHA")
+    solicitacao_medicao = baker.make(
+        "SolicitacaoMedicaoInicial",
+        uuid="0c914b27-c7cd-4682-a439-a4874745b005",   # agora válido
+        mes=12,
+        ano=2022,
+        escola=escola,
+        rastro_lote=escola.lote,
+        historico=json.dumps([]),
+    )
+    baker.make(
+        "Medicao",
+        solicitacao_medicao_inicial=solicitacao_medicao,
+        periodo_escolar=periodo_manha,
+    )
+    return solicitacao_medicao
 
 @pytest.fixture
 def solicitacao_medicao_inicial(escola, categoria_medicao, aluno):
@@ -773,6 +791,13 @@ def solicitacao_medicao_inicial_medicao_enviada_pela_ue_nok__2(
     solicitacao_medicao_inicial.save()
     return solicitacao_medicao_inicial
 
+@pytest.fixture
+def categoria_dieta_a():
+    return baker.make("CategoriaMedicao", nome="DIETA ESPECIAL - TIPO A")
+
+@pytest.fixture
+def categoria_dieta_b():
+    return baker.make("CategoriaMedicao", nome="DIETA ESPECIAL - TIPO B")
 
 @pytest.fixture
 def solicitacao_medicao_inicial_varios_valores(escola, categoria_medicao):
