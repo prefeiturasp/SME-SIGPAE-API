b7e6d8b8a7e3f6230bd79f364f6b0368d6613695
Priscyla C. dos Santos <santospriscyla@gmail.com>
2025-09-19 16:33:50 -0300

    test(127181):  Testa o método gerar_relatorio_reclamacao_produto_excel

--- sme_sigpae_api/produto/__tests__/utils/test_relatorio_reclamacao_produto.py
+++ sme_sigpae_api/produto/__tests__/utils/test_relatorio_reclamacao_produto.py
@@ -1,4 +1,8 @@
+import io
 from datetime import datetime
+
+from openpyxl import load_workbook
+import pandas as pd
 
 from sme_sigpae_api.produto.api.serializers.serializers import (
     ReclamacaoDeProdutoExcelSerializer,
@@ -6,6 +10,7 @@ from sme_sigpae_api.produto.api.serializers.serializers import (
 from sme_sigpae_api.produto.utils.relatorio_reclamacao_produto import (
     _extrair_dados_reclamacao,
     _gerar_subtitulo,
+    gerar_relatorio_reclamacao_produto_excel,
 )
 
 
@@ -83,3 +88,29 @@ def test_gerar_subtitulo_sem_datas():
         in resultado
     )
     assert "Período das Reclamações" not in resultado
+
+
+def test_gerar_relatorio_reclamacao_produto_excel(reclamacao_produto_query_excel):
+    reclamacoes = ReclamacaoDeProdutoExcelSerializer(
+        reclamacao_produto_query_excel, many=True
+    ).data
+    quantidade_reclamacoes = reclamacao_produto_query_excel.count()
+    filtros = {
+        "data_inicial_reclamacao": "01/01/2022",
+        "data_final_reclamacao": "01/09/2025",
+    }
+    output = gerar_relatorio_reclamacao_produto_excel(
+        reclamacoes, quantidade_reclamacoes, filtros
+    )
+
+    assert isinstance(output, io.BytesIO)    
+    workbook = load_workbook(filename=output)
+    nome_aba = f"Relatório Reclamação Produto"
+    assert nome_aba in workbook.sheetnames
+    sheet = workbook[nome_aba]
+    rows = list(sheet.iter_rows(values_only=True))
+    assert rows[0] == ('Relatório de Acompanhamento de Reclamação de Produtos', None, None, None, None, None, None, None, None, None, None, None, None)
+    assert rows[1] == ('Total de Reclamações de produtos para os editais selecionados: 3 | Período das Reclamações: De 01/01/2022 a 01/09/2025 | Data de Extração do Relatório: 19/09/2025', None, None, None, None, None, None, None, None, None, None, None, None)
+    assert rows[2] == (None, None, None, None, None, None, None, None, None, None, None, None, None)
+    assert rows[3] == ('Nº', 'Edital', 'DRE/LOTE', 'Empresa', 'Nome do Produto', 'Marca', 'Fabricante', 'Status do Produto', 'Nº da Reclamação', 'RF e Nome do Reclamante', 'Cód EOL e Nome da Escola', 'Status da Reclamação', 'Data da Reclamação')
+    
