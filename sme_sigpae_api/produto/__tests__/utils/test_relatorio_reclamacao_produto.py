e1f91fe29b7081cc8b7fc54f936c3c6c63f87321
Priscyla C. dos Santos <santospriscyla@gmail.com>
2025-09-19 16:13:17 -0300

    test(127181):  Testa o método gerar_subtitulo

--- sme_sigpae_api/produto/__tests__/utils/test_relatorio_reclamacao_produto.py
+++ sme_sigpae_api/produto/__tests__/utils/test_relatorio_reclamacao_produto.py
@@ -1,8 +1,11 @@
+from datetime import datetime
+
 from sme_sigpae_api.produto.api.serializers.serializers import (
     ReclamacaoDeProdutoExcelSerializer,
 )
 from sme_sigpae_api.produto.utils.relatorio_reclamacao_produto import (
     _extrair_dados_reclamacao,
+    _gerar_subtitulo,
 )
 
 
@@ -17,6 +20,66 @@ def test_extrair_dados_reclamacao(reclamacao_produto_query_excel):
     assert dado["Marca"] == "Marca1"
     assert dado["Fabricante"] == "Fabricante1"
     assert dado["Status do Produto"] == "RASCUNHO"
+    assert dado["Status da Reclamação"] == "Aguardando resposta da terceirizada"
     assert "Nº da Reclamação" in dado
     assert "RF e Nome do Reclamante" in dado
     assert "Data da Reclamação" in dado
+    assert "DRE/LOTE" in dado
+    assert "Empresa" in dado
+    assert "Cód EOL e Nome da Escola" in dado
+
+
+def test_gerar_subtitulo_com_datas():
+    filtros = {
+        "data_inicial_reclamacao": "01/01/2022",
+        "data_final_reclamacao": "01/09/2025",
+    }
+    resultado = _gerar_subtitulo(filtros, 3)
+    assert (
+        "Total de Reclamações de produtos para os editais selecionados: 3" in resultado
+    )
+    assert "Período das Reclamações: De 01/01/2022 a 01/09/2025" in resultado
+    assert (
+        f"Data de Extração do Relatório: {datetime.now().date().strftime('%d/%m/%Y')}"
+        in resultado
+    )
+
+
+def test_gerar_subtitulo_somente_data_inicial():
+    filtros = {
+        "data_inicial_reclamacao": "01/01/2022",
+    }
+    resultado = _gerar_subtitulo(filtros, 3)
+    assert (
+        "Total de Reclamações de produtos para os editais selecionados: 3" in resultado
+    )
+    assert "Período das Reclamações: A partir de 01/01/2022" in resultado
+    assert (
+        f"Data de Extração do Relatório: {datetime.now().date().strftime('%d/%m/%Y')}"
+        in resultado
+    )
+
+
+def test_gerar_subtitulo_somente_data_final():
+    filtros = {"data_final_reclamacao": "01/09/2025"}
+    resultado = _gerar_subtitulo(filtros, 3)
+    assert (
+        "Total de Reclamações de produtos para os editais selecionados: 3" in resultado
+    )
+    assert "Período das Reclamações: Até 01/09/2025" in resultado
+    assert (
+        f"Data de Extração do Relatório: {datetime.now().date().strftime('%d/%m/%Y')}"
+        in resultado
+    )
+
+
+def test_gerar_subtitulo_sem_datas():
+    resultado = _gerar_subtitulo({}, 3)
+    assert (
+        "Total de Reclamações de produtos para os editais selecionados: 3" in resultado
+    )
+    assert (
+        f"Data de Extração do Relatório: {datetime.now().date().strftime('%d/%m/%Y')}"
+        in resultado
+    )
+    assert "Período das Reclamações" not in resultado
