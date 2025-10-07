9cfcc159541e8b255e044191ac10395f21bd90fa
GuiLMassini <guimassinifai@gmail.com>
2025-10-07 11:25:52 -0300

    feat(134760): importacao dados reposicao cronograma

---
+++ utility/carga_dados/recebimento/importa_dados.py
@@ -0,0 +1,15 @@
+from sme_sigpae_api.recebimento.data.reposicao_cronograma import data_reposicao_cronograma
+from sme_sigpae_api.recebimento.models import ReposicaoCronogramaFichaRecebimento
+from utility.carga_dados.helper import ja_existe, progressbar
+
+
+def cria_reposicao_cronograma_ficha_recebimento():
+    for item in progressbar(
+        data_reposicao_cronograma, "Reposicao Cronograma Ficha Recebimento"
+    ):  # noqa
+        _, created = ReposicaoCronogramaFichaRecebimento.objects.get_or_create(
+            tipo=item["tipo"],
+            descricao=item["descricao"],
+        )
+        if not created:
+            ja_existe("ReposicaoCronogramaFichaRecebimento", item)
