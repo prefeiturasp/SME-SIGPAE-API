77caab4fb0e5d2bf890fc1ca980c1dd216b0913a
Calvin Rossinhole <calvin.masters@gmail.com>
2025-10-14 10:21:43 -0300

    fix(-): :bug: adiciona programas e projetos no dict

--- sme_sigpae_api/dados_comuns/constants.py
+++ sme_sigpae_api/dados_comuns/constants.py
@@ -440,8 +440,9 @@ ORDEM_HEADERS_CEMEI = {
     "Infantil INTEGRAL": 8,
     "Infantil MANHA": 9,
     "Infantil TARDE": 10,
-    "DIETA ESPECIAL - TIPO A - INFANTIL": 11,
-    "DIETA ESPECIAL - TIPO B - INFANTIL": 12,
+    "Programas e Projetos": 11,
+    "DIETA ESPECIAL - TIPO A - INFANTIL": 12,
+    "DIETA ESPECIAL - TIPO B - INFANTIL": 13,
 }
 
 ORDEM_HEADERS_EMEBS = {
