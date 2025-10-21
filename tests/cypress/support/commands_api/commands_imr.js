b4b84e003b1cba78e284648e526363862721b2ba
Leandro S Castro <leandro2509@gmail.com>
2025-10-21 10:42:52 -0300

    feat: add API commands for IMR Formulário Supervisão and related responses

--- tests/cypress/support/commands_api/commands_imr.js
+++ tests/cypress/support/commands_api/commands_imr.js
@@ -83,3 +83,99 @@ Cypress.Commands.add('consultar_imr_utensilios_mesa', (uuid) => {
 		failOnStatusCode: false,
 	})
 })
+
+Cypress.Commands.add(
+	'consultar_imr_formulario_supervisao_por_status',
+	(status) => {
+		cy.request({
+			method: 'GET',
+			url:
+				Cypress.config('baseUrl') +
+				`api/imr/formulario-supervisao/?status=${status}`,
+			timeout: 60000,
+			headers: {
+				Authorization: 'JWT ' + globalThis.token,
+			},
+			failOnStatusCode: false,
+		})
+	},
+)
+
+Cypress.Commands.add('consultar_imr_formulario_supervisao_por_uuid', (uuid) => {
+	cy.request({
+		method: 'GET',
+		url: Cypress.config('baseUrl') + `api/imr/formulario-supervisao/${uuid}/`,
+		timeout: 60000,
+		headers: {
+			Authorization: 'JWT ' + globalThis.token,
+		},
+		failOnStatusCode: false,
+	})
+})
+
+Cypress.Commands.add(
+	'consultar_imr_formulario_supervisao_respostas',
+	(uuid) => {
+		cy.request({
+			method: 'GET',
+			url:
+				Cypress.config('baseUrl') +
+				`api/imr/formulario-supervisao/${uuid}/respostas/`,
+			timeout: 60000,
+			headers: {
+				Authorization: 'JWT ' + globalThis.token,
+			},
+			failOnStatusCode: false,
+		})
+	},
+)
+
+Cypress.Commands.add(
+	'consultar_imr_formulario_supervisao_respostas_nao_aplica',
+	(uuid) => {
+		cy.request({
+			method: 'GET',
+			url:
+				Cypress.config('baseUrl') +
+				`api/imr/formulario-supervisao/${uuid}/respostas_nao_se_aplica/`,
+			timeout: 60000,
+			headers: {
+				Authorization: 'JWT ' + globalThis.token,
+			},
+			failOnStatusCode: false,
+		})
+	},
+)
+
+Cypress.Commands.add(
+	'consultar_imr_formulario_supervisao_dashboard',
+	(status) => {
+		cy.request({
+			method: 'GET',
+			url:
+				Cypress.config('baseUrl') + 'api/imr/formulario-supervisao/dashboard/',
+			timeout: 60000,
+			headers: {
+				Authorization: 'JWT ' + globalThis.token,
+			},
+			failOnStatusCode: false,
+		})
+	},
+)
+
+Cypress.Commands.add(
+	'consultar_imr_formulario_supervisao_lista_nomes_nutricionistas',
+	(status) => {
+		cy.request({
+			method: 'GET',
+			url:
+				Cypress.config('baseUrl') +
+				'api/imr/formulario-supervisao/lista_nomes_nutricionistas/',
+			timeout: 60000,
+			headers: {
+				Authorization: 'JWT ' + globalThis.token,
+			},
+			failOnStatusCode: false,
+		})
+	},
+)
