ce8dfb6366196700edd4c7043516125c45c33397
Calvin Rossinhole <calvin.masters@gmail.com>
2025-10-21 16:28:13 -0300

    test(135203): :white_check_mark: implementa teste unitario transferencia lote - inclusao cei

--- sme_sigpae_api/escola/__tests__/test_models/test_transferencia_lote.py
+++ sme_sigpae_api/escola/__tests__/test_models/test_transferencia_lote.py
@@ -14,19 +14,26 @@ from sme_sigpae_api.escola.fixtures.factories.escola_factory import (
     AlunosMatriculadosPeriodoEscolaFactory,
     DiretoriaRegionalFactory,
     EscolaFactory,
+    FaixaEtariaFactory,
     LogAlunosMatriculadosPeriodoEscolaFactory,
     LoteFactory,
     PeriodoEscolarFactory,
     TipoUnidadeEscolarFactory,
 )
 from sme_sigpae_api.inclusao_alimentacao.fixtures.factories.base_factory import (
+    DiasMotivosInclusaoDeAlimentacaoCEIFactory,
     GrupoInclusaoAlimentacaoNormalFactory,
     InclusaoAlimentacaoContinuaFactory,
+    InclusaoAlimentacaoDaCEIFactory,
     InclusaoAlimentacaoNormalFactory,
     MotivoInclusaoContinuaFactory,
+    QuantidadeDeAlunosPorFaixaEtariaDaInclusaoDeAlimentacaoDaCEIFactory,
     QuantidadePorPeriodoFactory,
 )
-from sme_sigpae_api.inclusao_alimentacao.models import GrupoInclusaoAlimentacaoNormal
+from sme_sigpae_api.inclusao_alimentacao.models import (
+    GrupoInclusaoAlimentacaoNormal,
+    InclusaoAlimentacaoDaCEI,
+)
 from sme_sigpae_api.perfil.fixtures.factories.perfil_base_factories import (
     UsuarioFactory,
 )
@@ -39,17 +46,31 @@ pytestmark = pytest.mark.django_db
 
 @freeze_time("2025-05-09")
 class TestUseCaseTransferenciaLotes:
-    def _setup_escola(self):
+    def _setup_dre_terc_lote(self):
         self.dre = DiretoriaRegionalFactory.create(nome="IPIRANGA", iniciais="IP")
         self.terceirizada = EmpresaFactory.create(nome_fantasia="ORIGINAL LTDA")
         self.lote = LoteFactory.create(
             nome="LOTE 01", diretoria_regional=self.dre, terceirizada=self.terceirizada
         )
+
+    def _setup_escola(self):
         self.tipo_unidade_emef = TipoUnidadeEscolarFactory.create(iniciais="EMEF")
         self.escola_emef = EscolaFactory.create(
             nome="EMEF PERICLES",
             tipo_gestao__nome="TERC TOTAL",
             tipo_unidade=self.tipo_unidade_emef,
+            lote=self.lote,
+            diretoria_regional=self.dre,
+        )
+
+    def _setup_escola_cei(self):
+        self.tipo_unidade_cei_diret = TipoUnidadeEscolarFactory.create(
+            iniciais="CEI DIRET"
+        )
+        self.escola_cei = EscolaFactory.create(
+            nome="CEI DIRET GERALDA",
+            tipo_gestao__nome="TERC TOTAL",
+            tipo_unidade=self.tipo_unidade_cei_diret,
             lote=self.lote,
             diretoria_regional=self.dre,
         )
@@ -134,7 +155,9 @@ class TestUseCaseTransferenciaLotes:
         )
 
     def _setup_testes(self):
+        self._setup_dre_terc_lote()
         self._setup_escola()
+        self._setup_escola_cei()
         self._setup_terceirizada_nova()
         self._setup_usuario()
         self._setup_periodos_escolares()
@@ -188,6 +211,55 @@ class TestUseCaseTransferenciaLotes:
         )
         LogSolicitacoesUsuarioFactory.create(
             uuid_original=self.grupo_inclusao_alimentacao_normal.uuid,
+            status_evento=LogSolicitacoesUsuario.CODAE_AUTORIZOU,
+            usuario=self.usuario,
+        )
+
+    def _setup_faixas_etarias(self):
+        self.faixa_etaria_1 = FaixaEtariaFactory.create(inicio=0, fim=1)
+        self.faixa_etaria_2 = FaixaEtariaFactory.create(inicio=1, fim=4)
+
+    def _setup_inclusao_cei(self):
+        self.inclusao_alimentacao_da_cei = InclusaoAlimentacaoDaCEIFactory.create(
+            escola=self.escola_cei,
+            rastro_dre=self.escola_cei.diretoria_regional,
+            rastro_lote=self.escola_cei.lote,
+            rastro_terceirizada=self.terceirizada,
+            status=InclusaoAlimentacaoDaCEI.workflow_class.CODAE_AUTORIZADO,
+        )
+        QuantidadeDeAlunosPorFaixaEtariaDaInclusaoDeAlimentacaoDaCEIFactory.create(
+            inclusao_alimentacao_da_cei=self.inclusao_alimentacao_da_cei,
+            faixa_etaria=self.faixa_etaria_1,
+            periodo=self.periodo_integral,
+            quantidade_alunos=10,
+        )
+        QuantidadeDeAlunosPorFaixaEtariaDaInclusaoDeAlimentacaoDaCEIFactory.create(
+            inclusao_alimentacao_da_cei=self.inclusao_alimentacao_da_cei,
+            faixa_etaria=self.faixa_etaria_2,
+            periodo=self.periodo_integral,
+            quantidade_alunos=20,
+        )
+        DiasMotivosInclusaoDeAlimentacaoCEIFactory.create(
+            inclusao_cei=self.inclusao_alimentacao_da_cei, data="2025-05-03"
+        )
+        DiasMotivosInclusaoDeAlimentacaoCEIFactory.create(
+            inclusao_cei=self.inclusao_alimentacao_da_cei, data="2025-05-13"
+        )
+        DiasMotivosInclusaoDeAlimentacaoCEIFactory.create(
+            inclusao_cei=self.inclusao_alimentacao_da_cei, data="2025-05-23"
+        )
+        LogSolicitacoesUsuarioFactory.create(
+            uuid_original=self.inclusao_alimentacao_da_cei.uuid,
+            status_evento=LogSolicitacoesUsuario.INICIO_FLUXO,
+            usuario=self.usuario,
+        )
+        LogSolicitacoesUsuarioFactory.create(
+            uuid_original=self.inclusao_alimentacao_da_cei.uuid,
+            status_evento=LogSolicitacoesUsuario.DRE_VALIDOU,
+            usuario=self.usuario,
+        )
+        LogSolicitacoesUsuarioFactory.create(
+            uuid_original=self.inclusao_alimentacao_da_cei.uuid,
             status_evento=LogSolicitacoesUsuario.CODAE_AUTORIZOU,
             usuario=self.usuario,
         )
@@ -261,6 +333,53 @@ class TestUseCaseTransferenciaLotes:
         assert inclusao_copia.quantidades_periodo.get().tipos_alimentacao.count() == 1
         assert inclusao_copia.inclusoes.count() == 2
         datas_copia = list(inclusao_copia.inclusoes.values_list("data", flat=True))
+        assert datetime.date(2025, 5, 13) in datas_copia
+        assert datetime.date(2025, 5, 23) in datas_copia
+        assert inclusao_copia.logs.count() == 3
+
+    def test_transferir_lote_inclusao_cei_data_aprovada_pos_transferencia(self):
+        self._setup_testes()
+        self._setup_faixas_etarias()
+        self._setup_inclusao_cei()
+
+        data_virada = datetime.date(2025, 5, 13)
+        terceirizada_pre_transferencia = self.terceirizada
+        terceirizada_nova = self.terceirizada_nova
+
+        self.lote._transferir_lote_lida_com_inclusoes_cei(
+            data_virada, terceirizada_pre_transferencia, terceirizada_nova
+        )
+
+        inclusoes_cei = (
+            self.lote.inclusao_alimentacao_inclusaoalimentacaodacei_rastro_lote.all()
+        )
+        assert inclusoes_cei.count() == 2
+
+        inclusao_original = inclusoes_cei.get(
+            uuid=self.inclusao_alimentacao_da_cei.uuid
+        )
+        assert inclusao_original.dias_motivos_da_inclusao_cei.count() == 1
+        datas_original = list(
+            inclusao_original.dias_motivos_da_inclusao_cei.values_list(
+                "data", flat=True
+            )
+        )
+        assert datetime.date(2025, 5, 3) in datas_original
+        assert datetime.date(2025, 5, 13) not in datas_original
+
+        inclusao_copia = inclusoes_cei.exclude(
+            uuid=self.inclusao_alimentacao_da_cei.uuid
+        ).get()
+        assert inclusao_copia.rastro_terceirizada == self.terceirizada_nova
+        assert inclusao_copia.quantidade_alunos_da_inclusao.count() == 2
+        assert (
+            inclusao_copia.quantidade_alunos_da_inclusao.first().faixa_etaria
+            is not None
+        )
+        assert inclusao_copia.dias_motivos_da_inclusao_cei.count() == 2
+        datas_copia = list(
+            inclusao_copia.dias_motivos_da_inclusao_cei.values_list("data", flat=True)
+        )
         assert datetime.date(2025, 5, 13) in datas_copia
         assert datetime.date(2025, 5, 23) in datas_copia
         assert inclusao_copia.logs.count() == 3
