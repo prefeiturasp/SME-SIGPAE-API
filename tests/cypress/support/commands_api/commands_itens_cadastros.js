e45b746acf9ebddbd952bb106ce25da04fe359c3
Leandro S Castro <leandro2509@gmail.com>
2025-11-07 17:29:37 -0300

    feat: update Cypress tests and commands for Itens Cadastros API

--- tests/cypress/support/commands_api/commands_itens_cadastros.js
+++ tests/cypress/support/commands_api/commands_itens_cadastros.js
@@ -59,3 +59,63 @@ Cypress.Commands.add('consultar_itens_cadastros_tipos', () => {
 		failOnStatusCode: false,
 	})
 })
+
+Cypress.Commands.add('cadastrar_itens_cadastros', (dados_teste) => {
+	cy.request({
+		method: 'POST',
+		url: Cypress.config('baseUrl') + 'api/itens-cadastros/',
+		timeout: 60000,
+		headers: {
+			Authorization: 'JWT ' + globalThis.token,
+		},
+		body: {
+			nome: dados_teste.nome,
+			tipo: dados_teste.tipo,
+		},
+		failOnStatusCode: false,
+	})
+})
+
+Cypress.Commands.add('deletar_itens_cadastros', (uuid) => {
+	cy.request({
+		method: 'DELETE',
+		url: Cypress.config('baseUrl') + `api/itens-cadastros/${uuid}/`,
+		timeout: 60000,
+		headers: {
+			Authorization: 'JWT ' + globalThis.token,
+		},
+		failOnStatusCode: false,
+	})
+})
+
+Cypress.Commands.add('put_alterar_itens_cadastros', (uuid, dados_teste) => {
+	cy.request({
+		method: 'PUT',
+		url: Cypress.config('baseUrl') + `api/itens-cadastros/${uuid}/`,
+		timeout: 60000,
+		headers: {
+			Authorization: 'JWT ' + globalThis.token,
+		},
+		body: {
+			nome: dados_teste.nome,
+			tipo: dados_teste.tipo,
+		},
+		failOnStatusCode: false,
+	})
+})
+
+Cypress.Commands.add('patch_alterar_itens_cadastros', (uuid, dados_teste) => {
+	cy.request({
+		method: 'PATCH',
+		url: Cypress.config('baseUrl') + `api/itens-cadastros/${uuid}/`,
+		timeout: 60000,
+		headers: {
+			Authorization: 'JWT ' + globalThis.token,
+		},
+		body: {
+			nome: dados_teste.nome,
+			tipo: dados_teste.tipo,
+		},
+		failOnStatusCode: false,
+	})
+})
