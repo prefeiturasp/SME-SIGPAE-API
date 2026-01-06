0378bc4ec2f170a431895fbc2bf0e28966e0e1ab
Leandro S Castro <leandro2509@gmail.com>
2026-01-06 13:52:33 -0300

    feat(api): add perfil validation tests and commands

--- tests/cypress/support/e2e.js
+++ tests/cypress/support/e2e.js
@@ -40,6 +40,7 @@ import './commands_api/commands_motivos_negacao'
 import './commands_api/commands_motivos_inclusao_normal'
 import './commands_api/commands_motivos_suspensao_cardapio'
 import './commands_api/commands_nome_de_produtos_edital'
+import './commands_api/commands_perfis'
 
 import './commands_ui/commands_login'
 import './commands_ui/commands_globais'
