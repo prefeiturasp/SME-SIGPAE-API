13bd2837a413c252b0a0b3396781023f980d3dc3
Priscyla C. dos Santos <santospriscyla@gmail.com>
2026-03-05 11:07:33 -0300

    test(141376): Adiciona  testes para logs de dietas recreio nas férias CEI

--- sme_sigpae_api/dieta_especial/__tests__/test_urls.py
+++ sme_sigpae_api/dieta_especial/__tests__/test_urls.py
@@ -1524,3 +1524,49 @@ def test_url_endpoint_verifica_dieta_log_inativo(
         log["status_evento_explicacao"] == "CODAE inativou"
         for log in informacao_dieta["logs"]
     )
+
+
+def test_logs_dieta_recreio_nas_ferias_cei(
+    client_autenticado_vinculo_codae_gestao_alimentacao_dieta,
+    escola,
+    logs_dieta_recreio_nas_ferias_cei,
+):
+    client, user = client_autenticado_vinculo_codae_gestao_alimentacao_dieta
+    response = client.get(
+        f"/log-quantidade-dietas-autorizadas-recreio-nas-ferias-cei/?escola_uuid={str(escola.uuid)}&mes=12&ano=2025"
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    logs = response.json()
+    assert len(logs) == 4
+    
+    logs_faixa_0_5 = [log for log in logs if log["faixa_etaria"]["__str__"] == "0 meses a 05 meses"]
+    assert len(logs_faixa_0_5) == 3
+    assert any(log["classificacao"] == "Tipo A" for log in logs_faixa_0_5)
+    assert any(log["classificacao"] == "Tipo A Enteral" for log in logs_faixa_0_5)
+    assert any(log["classificacao"] == "Tipo B" for log in logs_faixa_0_5)
+
+    logs_faixa_7_11 = [log for log in logs if log["faixa_etaria"]["__str__"] == "07 a 11 meses"]
+    assert len(logs_faixa_7_11) == 1
+    assert logs_faixa_7_11[0]["classificacao"] == "Tipo B"
+
+    for log in logs:
+        assert log["dia"] == "22"
+        assert log["data"] == "22/12/2025"
+        assert log["quantidade"] == 5
+        assert log["escola"] == str(escola.nome)
+        assert all(key in log["faixa_etaria"] for key in ["uuid", "inicio", "fim", "__str__"])
+
+
+def test_logs_dieta_recreio_nas_ferias_cei_escola_sem_log(
+    client_autenticado_vinculo_codae_gestao_alimentacao_dieta, escola
+):
+    client, user = client_autenticado_vinculo_codae_gestao_alimentacao_dieta
+    response = client.get(
+        f"/log-quantidade-dietas-autorizadas-recreio-nas-ferias-cei/?escola_uuid={str(escola.uuid)}&mes=12&ano=2025"
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    logs = response.json()
+    assert len(logs) == 0
+
