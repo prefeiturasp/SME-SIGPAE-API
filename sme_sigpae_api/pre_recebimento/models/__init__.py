7deef9d2a8bfd8c93a62a6836b27a80624659328
Luis Henrique Zimmermann <lhzimmermann@gmail.com>
2026-04-06 17:40:41 -0300

    fix(140558): fix flake8

--- sme_sigpae_api/pre_recebimento/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from sme_sigpae_api.pre_recebimento.cronograma_semanal.models import (
-    CronogramaSemanal,
-    ProgramacaoEntregaSemanal,
-)
-
-__all__ = [
-    "CronogramaSemanal",
-    "ProgramacaoEntregaSemanal",
-]
