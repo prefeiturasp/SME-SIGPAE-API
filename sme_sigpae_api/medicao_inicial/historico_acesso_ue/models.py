56371523264e30350f51ab444a24ed56f3f3b907
Calvin Rossinhole <calvin.masters@gmail.com>
2026-04-14 14:31:15 -0300

    feat(146799): :sparkles: implementa historico escola medicao manager

--- sme_sigpae_api/medicao_inicial/historico_acesso_ue/models.py
+++ sme_sigpae_api/medicao_inicial/historico_acesso_ue/models.py
@@ -1,4 +1,8 @@
+import calendar
+import datetime
+
 from django.db import models
+from django.db.models import Q
 
 from sme_sigpae_api.dados_comuns.behaviors import (
     CriadoEm,
@@ -8,9 +12,32 @@ from sme_sigpae_api.dados_comuns.behaviors import (
 )
 
 
+class HistoricoAcessoMedicaoInicialUEQuerySet(models.QuerySet):
+    def por_dre(self, dre_uuid):
+        return self.filter(escola__diretoria_regional__uuid=dre_uuid)
+
+    def ativos_no_mes_ano(self, mes: int, ano: int):
+        primeiro_dia_mes = datetime.date(ano, mes, 1)
+        ultimo_dia_mes = datetime.date(ano, mes, calendar.monthrange(ano, mes)[1])
+
+        return self.filter(
+            Q(
+                data_final__isnull=False,
+                data_inicial__lte=ultimo_dia_mes,
+                data_final__gte=primeiro_dia_mes,
+            )
+            | Q(
+                data_final__isnull=True,
+                data_inicial__lte=ultimo_dia_mes,
+            )
+        )
+
+
 class HistoricoAcessoMedicaoInicialUE(
     TemChaveExterna, TemIdentificadorExternoAmigavel, CriadoEm, CriadoPor
 ):
+    objects = HistoricoAcessoMedicaoInicialUEQuerySet.as_manager()
+
     escola = models.ForeignKey(
         "escola.Escola",
         on_delete=models.CASCADE,
