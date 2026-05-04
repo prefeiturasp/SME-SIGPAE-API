f41238dca7fba171d7a04080b63a57996be72b7f
Marcus Martins <marcusmartins38@gmail.com>
2026-05-04 17:48:38 -0400

    test(146337): incluir o email sme footer

--- src/dados_comuns/__tests__/test_templates/test_pre_recebimento_notificacao_alteracao_cronograma.py
+++ src/dados_comuns/__tests__/test_templates/test_pre_recebimento_notificacao_alteracao_cronograma.py
@@ -4,7 +4,7 @@ from django.template.loader import render_to_string
 def test_template_email_base():
     titulo = "Teste template"
     template = "pre_recebumento_notificacao_alteracao_cronograma.html"
-    hidden_email = None
+    hidden_email = False
     solicitacao = 12234
     usuario = "ADMINISTRADOR CODAE"
     nome_produto = "Arroz Tipo 1"
@@ -30,3 +30,4 @@ def test_template_email_base():
         f"O Fornecedor <strong>{usuario}</strong> Solicitou Alterações no Cronograma <strong>{solicitacao}</strong> de <strong>{nome_produto}</strong>"
         in html
     )
+    assert "smecodaesigpae@sme.prefeitura.sp.gov.br"
