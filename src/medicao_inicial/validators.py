06d359a4c3ac3e00f9b00d5f64b0441dbd44f86d
Calvin Rossinhole <calvin.masters@gmail.com>
2026-05-04 15:23:29 -0300

    fix(-): :bug: dia letivo so conta se tiver log

--- src/medicao_inicial/validators.py
+++ src/medicao_inicial/validators.py
@@ -52,17 +52,28 @@ calendario = BrazilSaoPauloCity()
 
 
 def get_lista_dias_letivos(solicitacao, escola, periodo_escolar=None):
+    mes = int(solicitacao.mes)
+    ano = int(solicitacao.ano)
     dias_letivos = DiaCalendario.objects.filter(
-        data__month=int(solicitacao.mes),
-        data__year=int(solicitacao.ano),
+        data__month=mes,
+        data__year=ano,
         escola=escola,
         dia_letivo=True,
         periodo_escolar=periodo_escolar,
     )
     dias_letivos = list(set(dias_letivos.values_list("data__day", flat=True)))
-    dias_letivos_uteis = filtrar_dias_letivos(
-        dias_letivos, int(solicitacao.mes), int(solicitacao.ano)
+    dias_letivos_uteis = filtrar_dias_letivos(dias_letivos, mes, ano)
+    dias_com_log = set(
+        LogAlunosMatriculadosPeriodoEscola.objects.filter(
+            escola=escola,
+            criado_em__year=ano,
+            criado_em__month=mes,
+            tipo_turma="REGULAR",
+            quantidade_alunos__gt=0,
+            periodo_escolar__isnull=False,
+        ).values_list("criado_em__day", flat=True)
     )
+    dias_letivos_uteis = [dia for dia in dias_letivos_uteis if dia in dias_com_log]
     return [
         str(dia) if not len(str(dia)) == 1 else ("0" + str(dia))
         for dia in dias_letivos_uteis
