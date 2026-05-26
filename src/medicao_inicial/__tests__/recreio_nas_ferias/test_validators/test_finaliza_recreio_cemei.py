b1750d1e4fefa0e25c58d35235ce625978a6c7bf
Priscyla C. dos Santos <santospriscyla@gmail.com>
2026-05-26 16:17:41 -0300

    test(147366): Inclui testes para validar as dietas

--- src/medicao_inicial/__tests__/recreio_nas_ferias/test_validators/test_finaliza_recreio_cemei.py
+++ src/medicao_inicial/__tests__/recreio_nas_ferias/test_validators/test_finaliza_recreio_cemei.py
@@ -1,10 +1,11 @@
-
-
 import pytest
 
 from src.medicao_inicial.models import ValorMedicao
-from src.medicao_inicial.recreio_nas_ferias.validators.recreio_cemei import validate_lancamento_alimentacoes_medicao_recreio_cemei
-
+from src.medicao_inicial.recreio_nas_ferias.validators.recreio_cemei import (
+    validate_lancamento_alimentacoes_medicao_recreio_cemei,
+    validate_lancamento_dietas_medicao_recreio_cemei,
+)
+from utility.carga_dados.perfil.importa_dados import Q
 
 pytestmark = pytest.mark.django_db
 GRUPO_CEI = "Recreio nas Férias - de 0 a 3 anos e 11 meses"
@@ -12,14 +13,16 @@ GRUPO_EMEI = "Recreio nas Férias - 4 a 14 anos"
 GRUPO_COLABORADORES = "Colaboradores"
 
 
-def test_validate_lancamento_alimentacoes_medicao_recreio_cemei(solicitacao_recreio_cemei):
+def test_validate_lancamento_alimentacoes_medicao_recreio_cemei(
+    solicitacao_recreio_cemei,
+):
 
     lista_erros = []
     validate_lancamento_alimentacoes_medicao_recreio_cemei(
         solicitacao_recreio_cemei, lista_erros
     )
     assert len(lista_erros) == 0
-    
+
 
 def test_validate_lancamento_alimentacoes_medicao_recreio_cemei_dados_nao_lancados_emei(
     solicitacao_recreio_cemei,
@@ -50,7 +53,7 @@ def test_validate_lancamento_alimentacoes_medicao_recreio_cemei_dados_nao_lancad
 
     for esperado in erros_esperados:
         assert esperado in lista_erros, f"Elemento {esperado} não encontrado"
-        
+
 
 def test_validate_lancamento_alimentacoes_medicao_recreio_cemei_dados_nao_lancados_cei(
     solicitacao_recreio_cemei, categoria_medicao
@@ -59,7 +62,7 @@ def test_validate_lancamento_alimentacoes_medicao_recreio_cemei_dados_nao_lancad
         medicao__grupo__nome__in=[GRUPO_CEI, GRUPO_COLABORADORES],
         nome_campo="frequencia",
         dia="17",
-        categoria_medicao=categoria_medicao
+        categoria_medicao=categoria_medicao,
     )
     assert valores.count() == 9
     valores.delete()
@@ -84,8 +87,8 @@ def test_validate_lancamento_alimentacoes_medicao_recreio_cemei_dados_nao_lancad
 
     for esperado in erros_esperados:
         assert esperado in lista_erros, f"Elemento {esperado} não encontrado"
-        
-    
+
+
 def test_validate_lancamento_alimentacoes_medicao_recreio_cemei_dados_nao_lancados_geral(
     solicitacao_recreio_cemei, categoria_medicao
 ):
@@ -93,7 +96,7 @@ def test_validate_lancamento_alimentacoes_medicao_recreio_cemei_dados_nao_lancad
         medicao__solicitacao_medicao_inicial=solicitacao_recreio_cemei,
         nome_campo="frequencia",
         dia="17",
-        categoria_medicao=categoria_medicao
+        categoria_medicao=categoria_medicao,
     )
     assert valores.count() == 10
     valores.delete()
@@ -117,6 +120,101 @@ def test_validate_lancamento_alimentacoes_medicao_recreio_cemei_dados_nao_lancad
         {
             "erro": "Restam dias a serem lançados nas alimentações.",
             "periodo_escolar": "Recreio nas Férias - 4 a 14 anos",
+        },
+    ]
+
+    for esperado in erros_esperados:
+        assert esperado in lista_erros, f"Elemento {esperado} não encontrado"
+
+
+def test_validate_lancamento_dietas_medicao_recreio_cemei(solicitacao_recreio_cemei):
+    lista_erros = []
+    validate_lancamento_dietas_medicao_recreio_cemei(
+        solicitacao_recreio_cemei, lista_erros
+    )
+    assert len(lista_erros) == 0
+
+
+def test_validate_lancamento_dietas_medicao_recreio_cemei_dados_nao_lancados_emei(
+    solicitacao_recreio_cemei, categoria_medicao_dieta_a_enteral_aminoacidos
+):
+    valores = ValorMedicao.objects.filter(
+        medicao__solicitacao_medicao_inicial=solicitacao_recreio_cemei,
+        nome_campo="refeicao",
+        dia="17",
+        categoria_medicao=categoria_medicao_dieta_a_enteral_aminoacidos,
+    )
+    assert valores.count() == 1
+    valores.delete()
+    assert valores.count() == 0
+    lista_erros = []
+    validate_lancamento_dietas_medicao_recreio_cemei(
+        solicitacao_recreio_cemei, lista_erros
+    )
+    assert len(lista_erros) == 1
+    assert lista_erros == [
+        {
+            "erro": "Restam dias a serem lançados nas dietas.",
+            "periodo_escolar": "Recreio nas Férias - 4 a 14 anos",
+        }
+    ]
+
+
+def test_validate_lancamento_dietas_medicao_recreio_cemei_dados_nao_lancados_cei(
+    solicitacao_recreio_cemei, categoria_medicao_dieta_a
+):
+    valores = ValorMedicao.objects.filter(
+        medicao__solicitacao_medicao_inicial=solicitacao_recreio_cemei,
+        nome_campo="frequencia",
+        dia="17",
+        categoria_medicao=categoria_medicao_dieta_a,
+    )
+    assert valores.count() == 8
+    valores.delete()
+    assert valores.count() == 0
+    lista_erros = []
+    validate_lancamento_dietas_medicao_recreio_cemei(
+        solicitacao_recreio_cemei, lista_erros
+    )
+    assert len(lista_erros) == 1
+    assert lista_erros == [
+        {
+            "erro": "Restam dias a serem lançados nas dietas.",
+            "periodo_escolar": "Recreio nas Férias - de 0 a 3 anos e 11 meses",
+        }
+    ]
+
+
+def test_validate_lancamento_dietas_medicao_recreio_cemei_dados_nao_lancados_geral(
+    solicitacao_recreio_cemei,
+    categoria_medicao_dieta_a,
+    categoria_medicao_dieta_a_enteral_aminoacidos,
+):
+    valores = ValorMedicao.objects.filter(
+        medicao__solicitacao_medicao_inicial=solicitacao_recreio_cemei,
+        dia="17",
+        nome_campo__in=["frequencia", "refeicao"],
+        categoria_medicao__in=[
+            categoria_medicao_dieta_a,
+            categoria_medicao_dieta_a_enteral_aminoacidos,
+        ],
+    )
+    assert valores.count() == 10
+    valores.delete()
+    assert valores.count() == 0
+    lista_erros = []
+    lista_erros = validate_lancamento_dietas_medicao_recreio_cemei(
+        solicitacao_recreio_cemei, lista_erros
+    )
+    assert len(lista_erros) == 2
+    erros_esperados = [
+        {
+            "erro": "Restam dias a serem lançados nas dietas.",
+            "periodo_escolar": "Recreio nas Férias - 4 a 14 anos",
+        },
+        {
+            "erro": "Restam dias a serem lançados nas dietas.",
+            "periodo_escolar": "Recreio nas Férias - de 0 a 3 anos e 11 meses",
         },
     ]
 
