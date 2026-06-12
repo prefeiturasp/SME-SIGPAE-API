a81778d45b74cbb0fbf54bf194c9847919669e46
marcusmartins <marcusmartins38@gmail.com>
2026-06-12 16:48:59 -0300

    test(139052): validacao rodape pdf

--- src/relatorios/__tests__/test_relatorios_.py
+++ src/relatorios/__tests__/test_relatorios_.py
@@ -1066,3 +1066,20 @@ def test_relatorio_solicitacao_medicao_recreio_nas_ferias_rodape_aprovacao(
     assert "Aprovado por CODAE em" in texto
     assert "05/08/2025" in texto
     assert "Usuário TESTE" in texto
+
+
+def test_relatorio_solicitacao_medicao_recreio_nas_ferias_cei_rodape_aprovacao(
+    solicitacao_medicao_inicial_recreio_nas_ferias_cei_aprovada_codae,
+):
+    relatorio = relatorio_solicitacao_medicao_por_escola_recreio_nas_ferias(
+        solicitacao_medicao_inicial_recreio_nas_ferias_cei_aprovada_codae
+    )
+    texto = extrair_texto_de_pdf(relatorio)
+
+    assert "INFORMAÇÕES BÁSICAS DA MEDIÇÃO" in texto
+    assert "CEI DIRET JOAO MENDES" in texto
+    assert "Recreio nas Férias - Julho 2025" in texto
+    assert "SOMATÓRIO DE ALIMENTAÇÕES E DIETAS OFERTADAS" in texto
+    assert "Aprovado por CODAE em" in texto
+    assert "05/08/2025" in texto
+    assert "Usuário TESTE" in texto
