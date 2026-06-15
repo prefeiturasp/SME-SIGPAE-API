c4290dd654dd1f0518941fc1b351a1f73cdb735d
Calvin Rossinhole <calvin.masters@gmail.com>
2026-06-15 15:24:54 -0300

    refactor(150512): :fire: remove form inutilizado

--- src/dieta_especial/forms.py
+++ src/dieta_especial/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 
-from ..escola.models import DiretoriaRegional, Escola, Lote
-from ..terceirizada.models import Terceirizada
-from .protocolo_padrao.models import ProtocoloPadraoDietaEspecial
+from ..escola.models import DiretoriaRegional, Escola
 from .solicitacao_dieta_especial.models import (
     AlergiaIntolerancia,
     ClassificacaoDieta,
@@ -66,31 +64,6 @@ class RelatorioDietaForm(forms.Form):
     )
     diagnostico = forms.ModelMultipleChoiceField(
         required=False, queryset=AlergiaIntolerancia.objects.all()
-    )
-    data_inicial = forms.DateField(required=False)
-    data_final = forms.DateField(required=False)
-
-
-class RelatorioDietaTerceirizadaForm(forms.Form):
-    terceirizada_uuid = forms.ModelChoiceField(
-        required=False, queryset=Terceirizada.objects.all(), to_field_name="uuid"
-    )
-    status = forms.ChoiceField(
-        required=False,
-        choices=(
-            ("AUTORIZADAS", "Autorizadas"),
-            ("CANCELADAS", "Canceladas"),
-        ),
-    )
-    lote = forms.ModelMultipleChoiceField(
-        required=False,
-        queryset=Lote.objects.all(),
-    )
-    classificacao_dieta = forms.ModelMultipleChoiceField(
-        required=False, queryset=ClassificacaoDieta.objects.all()
-    )
-    protocolo_padrao = forms.ModelMultipleChoiceField(
-        required=False, queryset=ProtocoloPadraoDietaEspecial.objects.all()
     )
     data_inicial = forms.DateField(required=False)
     data_final = forms.DateField(required=False)
