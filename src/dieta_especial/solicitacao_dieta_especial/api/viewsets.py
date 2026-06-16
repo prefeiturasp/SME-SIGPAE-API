6638864ceca0a5fcbc04f79ae2a8f22c843011df
Calvin Rossinhole <calvin.masters@gmail.com>
2026-06-15 22:38:38 -0300

    refactor(150512): :fire: move forms

--- src/dieta_especial/solicitacao_dieta_especial/api/viewsets.py
+++ src/dieta_especial/solicitacao_dieta_especial/api/viewsets.py
@@ -42,12 +42,7 @@ from src.dados_comuns.permissions import (
 )
 from src.dados_comuns.services import enviar_email_codae_atualiza_protocolo
 from src.dados_comuns.utils import convert_dict_to_querydict
-from src.dieta_especial.forms import (
-    NegaDietaEspecialForm,
-    RelatorioDietaForm,
-    RelatorioQuantitativoSolicDietaEspForm,
-    SolicitacoesAtivasInativasPorAlunoForm,
-)
+from src.dieta_especial.forms import RelatorioDietaForm
 from src.dieta_especial.protocolo_padrao.api.serializers import (
     AlimentoSerializer,
 )
@@ -60,6 +55,11 @@ from src.dieta_especial.solicitacao_dieta_especial.api.filters import (
     AlimentoFilter,
     DietaEspecialFilter,
     MotivoNegacaoFilter,
+)
+from src.dieta_especial.solicitacao_dieta_especial.api.forms import (
+    NegaDietaEspecialForm,
+    RelatorioQuantitativoSolicDietaEspForm,
+    SolicitacoesAtivasInativasPorAlunoForm,
 )
 from src.dieta_especial.solicitacao_dieta_especial.api.serializers import (
     AlergiaIntoleranciaSerializer,
