02e04604da534fec656765db1acf603293a86b62
Marcus Martins <marcusmartins38@gmail.com>
2026-07-02 10:41:50 -0400

    feat(148610): viewset de update

--- src/escola/dias_letivos/api/viewsets.py
+++ src/escola/dias_letivos/api/viewsets.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from django_filters import rest_framework as filters
 from rest_framework import status
-from rest_framework.mixins import CreateModelMixin, ListModelMixin
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
@@ -13,12 +13,12 @@ from ....dados_comuns.permissions import (
 )
 from ..models import DiaLetivoSIGPAE
 from .filters import DiaLetivoFilter
-from .serializers import DiaLetivoSerializer
-from .serializers_create import DiaLetivoCreateSerializer
+from .serializers import DiaLetivoSerializer, DiaLetivoDetailSerializer
+from .serializers_create import DiaLetivoCreateSerializer, DiaLetivoUpdateSerializer
 
 
 class DiaLetivoViewSet(
-    ViewSetActionPermissionMixin, ListModelMixin, CreateModelMixin, GenericViewSet
+    ViewSetActionPermissionMixin, ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet
 ):
     """ViewSet para criação e listagem de dias letivos no SIGPAE.
 
@@ -32,6 +32,9 @@ class DiaLetivoViewSet(
     permission_action_classes = {
         "create": [UsuarioCODAEGestaoAlimentacao],
         "list": [UsuarioCODAEGestaoAlimentacao],
+        "retrieve": [UsuarioCODAEGestaoAlimentacao],
+        "update": [UsuarioCODAEGestaoAlimentacao],
+        "partial_update": [UsuarioCODAEGestaoAlimentacao],
     }
     queryset = DiaLetivoSIGPAE.objects.prefetch_related(
         "lotes__contratos_do_lote__edital",
@@ -42,6 +45,7 @@ class DiaLetivoViewSet(
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = DiaLetivoFilter
     pagination_class = None
+    lookup_field = "uuid"
 
     def get_serializer_class(self):
         """Retorna o serializador adequado conforme a ação.
@@ -51,7 +55,16 @@ class DiaLetivoViewSet(
         """
         if self.action == "list":
             return DiaLetivoSerializer
+        if self.action == "retrieve":
+            return DiaLetivoDetailSerializer
+        if self.action in ("update", "partial_update"):
+            return DiaLetivoUpdateSerializer
         return DiaLetivoCreateSerializer
+
+    def filter_queryset(self, queryset):
+        if self.action == "list":
+            return super().filter_queryset(queryset)
+        return queryset
 
     def create(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         """Cria novos dias letivos a partir de recorrências e retorna HTTP 201.
@@ -63,3 +76,10 @@ class DiaLetivoViewSet(
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(status=status.HTTP_201_CREATED)
+
+    def update(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        instance = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.update(instance, serializer.validated_data)
+        return Response(status=status.HTTP_200_OK)
