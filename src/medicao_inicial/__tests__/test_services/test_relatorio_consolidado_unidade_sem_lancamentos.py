8b07f926a7f8f1b97ff26442240d7d32c1c59d09
Priscyla C. dos Santos <santospriscyla@gmail.com>
2026-07-30 18:49:42 -0300

    test(152513): adiciona teste para o método insere_tabela_periodos_na_planilha

--- src/medicao_inicial/__tests__/test_services/test_relatorio_consolidado_unidade_sem_lancamentos.py
+++ src/medicao_inicial/__tests__/test_services/test_relatorio_consolidado_unidade_sem_lancamentos.py
@@ -1,8 +1,11 @@
+from io import BytesIO
+import pandas as pd
 import pytest
 
 from src.medicao_inicial.services.relatorio_consolidado_emei_emef import (
     get_alimentacoes_por_periodo,
     get_valores_tabela,
+    insere_tabela_periodos_na_planilha,
 )
 
 pytestmark = pytest.mark.django_db
@@ -40,3 +43,46 @@ def test_get_valores_tabela_unidade_emei(
     assert isinstance(linhas[0], list)
     assert len(linhas[0]) == 5
     assert linhas[0] == ['EMEF', '123456', 'EMEF TESTE', 'SL', 'SL']
+    
+def test_insere_tabela_periodos_na_planilha_unidade_emei(
+    solicitacao_sem_lancamento
+):
+    colunas    = [('MANHA', 'total_refeicoes_pagamento'), ('MANHA', 'total_sobremesas_pagamento')]
+    linhas = [['EMEF', '123456', 'EMEF TESTE', 'SL', 'SL']]
+    arquivo = BytesIO()
+    aba = f"Relatório Consolidado {solicitacao_sem_lancamento.mes}-{ solicitacao_sem_lancamento.ano}"
+    writer = pd.ExcelWriter(arquivo, engine="xlsxwriter")
+
+    df = insere_tabela_periodos_na_planilha(aba, colunas, linhas, writer)
+    assert isinstance(df, pd.DataFrame)
+    colunas_df = df.columns.tolist()
+    assert len(colunas_df) == 5
+    assert sum(1 for tupla in colunas_df if tupla[0] == "MANHA") == 2
+    assert sum(1 for tupla in colunas_df if tupla[0] == "DIETA ESPECIAL - TIPO A") == 0
+    assert sum(1 for tupla in colunas_df if tupla[0] == "DIETA ESPECIAL - TIPO B") == 0
+    assert sum(1 for tupla in colunas_df if tupla[1] == "Tipo") == 1
+    assert sum(1 for tupla in colunas_df if tupla[1] == "Cód. EOL") == 1
+    assert sum(1 for tupla in colunas_df if tupla[1] == "Unidade Escolar") == 1
+    assert sum(1 for tupla in colunas_df if tupla[1] == "Kit Lanche") == 0
+    assert sum(1 for tupla in colunas_df if tupla[1] == "Lanche Emerg.") ==0
+    assert sum(1 for tupla in colunas_df if tupla[1] == "Lanche") == 0
+    assert sum(1 for tupla in colunas_df if tupla[1] == "Lanche 4h") ==0
+    assert sum(1 for tupla in colunas_df if tupla[1] == "Refeição") ==0
+    assert (
+        sum(
+            1 for tupla in colunas_df if tupla[1] == "Total de Refeições para Pagamento"
+        )
+        == 1
+    )
+    assert sum(1 for tupla in colunas_df if tupla[1] == "Sobremesa") == 0
+    assert (
+        sum(
+            1
+            for tupla in colunas_df
+            if tupla[1] == "Total de Sobremesas para Pagamento"
+        )
+        == 1
+    )
+
+    assert df.iloc[0].tolist() == ['EMEF', '123456', 'EMEF TESTE', 'SL', 'SL']
+    assert df.iloc[1].tolist() == [0.0, 123456.0, 0.0, 0.0, 0.0]
