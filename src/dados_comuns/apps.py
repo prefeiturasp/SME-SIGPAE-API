fc6bbd155728b056a92604b21de0c34be6897736
Calvin Rossinhole <calvin.masters@gmail.com>
2026-09-10 11:25:13 -0300

    refactor(155831): :hammer: move inicializacao do sidecar para perfil

--- src/dados_comuns/apps.py
+++ src/dados_comuns/apps.py
@@ -8,14 +8,3 @@ class DadosComunsConfig(AppConfig):
 
     def ready(self) -> None:
         importlib.import_module("src.dados_comuns.signals")
-
-        """Inicializa os recursos compartilhados da SDK."""
-        from sme_sidecar_sdk import runtime
-        from sme_sidecar_sdk.config import Settings
-
-        runtime.configure(
-            Settings(
-                service_name="sigpae",
-                service_version="1.0.0",
-            )
-        )
