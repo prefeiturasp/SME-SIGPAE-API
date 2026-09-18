5f3961403348de540236a6ef5e06b3b47bf13899
Evelyn Cristina Brocardo <evelyn.brocardo@spassu.com.br>
2026-09-18 11:02:46 -0300

    teste automaçao api/analise_sensorial

--- tests/cypress/support/commands_api/commands_analise_sensorial.js
+++ tests/cypress/support/commands_api/commands_analise_sensorial.js
@@ -23,3 +23,28 @@ Cypress.Commands.add('consultar_analise_sensorial_com_filtros', (filtro) => {
 		failOnStatusCode: false,
 	})
 })
+
+Cypress.Commands.add('requisitar_analise_sensorial', (metodo, caminho = '', dados, autenticado = true) => {
+	return cy.request({
+		method: metodo,
+		url: `${Cypress.config('baseUrl')}api/analise-sensorial/${caminho}`,
+		body: dados,
+		headers: autenticado ? { Authorization: `JWT ${globalThis.token}` } : {},
+		timeout: 60000,
+		failOnStatusCode: false,
+	})
+})
+
+Cypress.Commands.add('consultar_referencia_analise_sensorial', () => {
+	return cy.request({
+		url: `${Cypress.config('baseUrl')}api/produtos/filtro-relatorio-em-analise-sensorial/`,
+		qs: { limit: 1 },
+		headers: { Authorization: `JWT ${globalThis.token}` },
+		timeout: 60000,
+		failOnStatusCode: false,
+	}).then((response) => {
+		expect(response.status).to.eq(200)
+		expect(response.body.results).to.be.an('array').and.not.be.empty
+		return response.body.results[0].ultima_homologacao
+	})
+})
